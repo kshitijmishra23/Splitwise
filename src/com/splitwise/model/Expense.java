8756f8b18b74de274c0ddfb915c43c306d2c1c11
Kshitij Mishra <kshtjmishra23@gmail.com>
2021-06-28 21:24:31 +0530

    add expense methods added

--- src/com/splitwise/model/Expense.java
+++ src/com/splitwise/model/Expense.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class Expense extends AuditableEntity{
-	private static Long ID = 0L;
+	private static Long ID = 1L;
 	private String name;
 	private String description;
 	
@@ -16,18 +16,23 @@ public class Expense extends AuditableEntity{
 	
 	private HashMap<User, Double> amountPaid;
 	private HashMap<User, Double> amountOwned;
+	private Date date;
 	
 	
-	public Expense(Long id, String name, String description, User createdBy, Set<User> participants, Double totalAmount,
-			HashMap<User, Double> amountPaid, HashMap<User, Double> amountOwned) {
+	public Expense(String name, String description, User createdBy, Set<User> participants, Double totalAmount) {
 		super(ID++);
 		this.name = name;
 		this.description = description;
 		this.createdBy = createdBy;
 		this.participants = participants;
 		this.totalAmount = totalAmount;
-		this.amountPaid = amountPaid;
-		this.amountOwned = amountOwned;
+		this.amountPaid =  new HashMap<User, Double>();
+		this.amountOwned = new HashMap<User, Double>();
+		this.date = new Date();
+		for(User participant : participants) {
+			participant.addExpense(this);
+		}
+	
 	}
 	public String getName() {
 		return name;
